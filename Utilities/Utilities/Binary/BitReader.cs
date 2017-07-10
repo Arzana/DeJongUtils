17a796fa145bba0e70f127d6f7cf4070be776482
Frank <largewowfan@gmail.com>
2017-07-10 22:17:36 +0200

    - Added binary reader and writer

---
+++ Utilities/Utilities/Binary/BitReader.cs
@@ -0,0 +1,129 @@
+﻿namespace DeJong.Utilities.Binary
+{
+    using System;
+    using Core;
+
+    /// <summary>
+    /// Contains general functions used for reading binary data.
+    /// </summary>
+#if !DEBUG
+    [System.Diagnostics.DebuggerStepThrough]
+#endif
+    public static class BitReader
+    {
+        /// <summary>
+        /// Read a full or partial byte from a specified buffer.
+        /// </summary>
+        /// <param name="source"> The source buffer. </param>
+        /// <param name="offset"> The starting point (in bits). </param>
+        /// <param name="length"> The amount of bits to read. </param>
+        /// <returns> The bits that have been read in a byte. </returns>
+        /// <remarks>
+        /// Check if length in withing range
+        /// Check if the offset if byte alligned and we need the full byte (possible return)
+        /// Get the needed part of the first byte
+        /// Check if the next byte is needed (possible return)
+        /// Get the needed part of the next byte
+        /// Compute result byte
+        /// </remarks>
+        public static byte ReadByte(byte[] source, int offset, int length)
+        {
+            CheckOverflow(nameof(ReadByte), length, 8);
+
+            int byteIndex = offset >> 3;
+            int start = offset - (byteIndex << 3);
+            if (start == 0 && length == 8) return source[byteIndex];
+
+            byte result = (byte)(source[byteIndex] >> start);
+            int bitsInNextByte = length - (8 - start);
+            if (bitsInNextByte < 1) return (byte)(result & (255 >> (8 - length)));
+
+            byte nextByte = source[byteIndex + 1];
+            nextByte &= (byte)(255 >> (8 - bitsInNextByte));
+            return (byte)(result | (byte)(nextByte << (length - bitsInNextByte)));
+        }
+
+        /// <summary>
+        /// Reads a full or partial ushort from a specified buffer.
+        /// </summary>
+        /// <param name="source"> The source buffer. </param>
+        /// <param name="offset"> The starting point (int bits). </param>
+        /// <param name="length"> The amount of bits to read. </param>
+        /// <returns> The bits that have been read in a ushort. </returns>
+        public static ushort ReadUInt16(byte[] source, int offset, int length)
+        {
+            CheckOverflow(nameof(ReadUInt16), length, 16);
+            return (ushort)ReadVariableBytes(source, offset, length, sizeof(ushort));
+        }
+
+        /// <summary>
+        /// Reads a full or partial uint from a specified buffer.
+        /// </summary>
+        /// <param name="source"> The source buffer. </param>
+        /// <param name="offset"> The starting point (int bits). </param>
+        /// <param name="length"> The amount of bits to read. </param>
+        /// <returns> The bits that have been read in a uint. </returns>
+        public static uint ReadUInt32(byte[] source, int offset, int length)
+        {
+            CheckOverflow(nameof(ReadUInt32), length, 32);
+            return (uint)ReadVariableBytes(source, offset, length, sizeof(uint));
+        }
+
+        /// <summary>
+        /// Reads a full or partial ulong from a specified buffer.
+        /// </summary>
+        /// <param name="source"> The source buffer. </param>
+        /// <param name="offset"> The starting point (int bits). </param>
+        /// <param name="length"> The amount of bits to read. </param>
+        /// <returns> The bits that have been read in a ulong. </returns>
+        public static ulong ReadUInt64(byte[] source, int offset, int length)
+        {
+            CheckOverflow(nameof(ReadUInt64), length, 64);
+            return ReadVariableBytes(source, offset, length, sizeof(ulong));
+        }
+
+        /// <summary>
+        /// Reads a specified amount of bytes from a specified buffer.
+        /// </summary>
+        /// <param name="source"> The source bufer. </param>
+        /// <param name="srcOffset"> The starting point (in bits). </param>
+        /// <param name="length"> The amount of bytes to read. </param>
+        /// <param name="destination"> The destination buffer. </param>
+        /// <param name="destOffset"> The starting point (in bytes). </param>
+        public static void ReadBytes(byte[] source, int srcOffset, int length, byte[] destination, int destOffset)
+        {
+            int byteIndex = srcOffset >> 3;
+            int start = srcOffset - (byteIndex << 3);
+
+            if (start == 0) Buffer.BlockCopy(source, srcOffset, destination, destOffset, length);
+            else
+            {
+                int nextPartLen = 8 - start;
+                int nextMask = 255 >> nextPartLen;
+
+                for (int i = 0; i < length; i++)
+                {
+                    int first = source[byteIndex++] >> start;
+                    int second = source[byteIndex] & nextMask;
+                    destination[destOffset++] = (byte)(first | (second << nextPartLen));
+                }
+            }
+        }
+
+        private static ulong ReadVariableBytes(byte[] source, int offset, int length, int numBytes)
+        {
+            ulong result = 0;
+            for (int i = 0; i < numBytes && length > 0; i++, length -= 8, offset += 8)
+            {
+                if (length <= 8) result |= (ulong)ReadByte(source, offset, length) << (i << 3);
+                else result |= (ulong)ReadByte(source, offset, 8) << (i << 3);
+            }
+            return result;
+        }
+
+        private static void CheckOverflow(string func, int check, int max)
+        {
+            LoggedException.RaiseIf(check < 1 || check > max, nameof(BitReader), $"{func} can only read between 1 and {max} bits");
+        }
+    }
+}
---
+++ Utilities/Utilities/Binary/BitWriter.cs
@@ -0,0 +1,133 @@
+﻿namespace DeJong.Utilities.Binary
+{
+    using Core;
+    using System;
+
+    /// <summary>
+    /// Contains general functions for writing binary data.
+    /// </summary>
+#if !DEBUG
+    [System.Diagnostics.DebuggerStepThrough]
+#endif
+    public static class BitWriter
+    {
+        /// <summary>
+        /// Writes a specified amount of bits to the specified buffer.
+        /// </summary>
+        /// <param name="destination"> The destination buffer. </param>
+        /// <param name="value"> The container pf the value. </param>
+        /// <param name="offset"> The staring point (in bits). </param>
+        /// <param name="length"> The amount of bits to write. </param>
+        /// <remarks>
+        /// Check if length is within range
+        /// Get the needed part of the value
+        /// Check if a second byte is needed
+        /// Calculate mask(s)
+        /// Set byte in destination
+        /// </remarks>
+        public static void WriteByte(byte[] destination, byte value, int offset, int length)
+        {
+            CheckOverflow(nameof(WriteByte), length, 8);
+            value = (byte)(value & (255 >> (8 - length)));
+
+            int byteIndex = offset >> 3;
+            int usedBits = offset & 7;
+            int freeBits = 8 - usedBits;
+            int bitsLeft = freeBits - length;
+
+            if (bitsLeft >= 0)
+            {
+                int mask = (255 >> freeBits) | (255 << (8 - bitsLeft));
+                destination[byteIndex] = (byte)((destination[byteIndex] & mask) | (value << usedBits));
+            }
+            else
+            {
+                destination[byteIndex] = (byte)((destination[byteIndex] & (255 >> freeBits)) | (value << usedBits));
+                destination[++byteIndex] = (byte)((destination[byteIndex] & (255 - freeBits)) | (value >> freeBits));
+            }
+        }
+
+        /// <summary>
+        /// Writes a full or partial ushort to the specified buffer.
+        /// </summary>
+        /// <param name="destination"> The destination buffer. </param>
+        /// <param name="value"> The container for the value. </param>
+        /// <param name="offset"> The starting point (in bits). </param>
+        /// <param name="length"> The amount of bits to write. </param>
+        public static void WriteUInt16(byte[] destination, ushort value, int offset, int length)
+        {
+            CheckOverflow(nameof(WriteUInt16), length, 16);
+            WriteVariableBytes(destination, value, offset, length, sizeof(ushort));
+        }
+
+        /// <summary>
+        /// Writes a full or partial uint to the specified buffer.
+        /// </summary>
+        /// <param name="destination"> The destination buffer. </param>
+        /// <param name="value"> The container for the value. </param>
+        /// <param name="offset"> The starting point (in bits). </param>
+        /// <param name="length"> The amount of bits to write. </param>
+        public static void WriteUInt32(byte[] destination, uint value, int offset, int length)
+        {
+            CheckOverflow(nameof(WriteUInt32), length, 32);
+            WriteVariableBytes(destination, value, offset, length, sizeof(int));
+        }
+
+        /// <summary>
+        /// Writes a full or partial ulong to the specified buffer.
+        /// </summary>
+        /// <param name="destination"> The destination buffer. </param>
+        /// <param name="value"> The container for the value. </param>
+        /// <param name="offset"> The starting point (in bits). </param>
+        /// <param name="length"> The amount of bits to write. </param>
+        public static void WriteUInt64(byte[] destination, ulong value, int offset, int length)
+        {
+            CheckOverflow(nameof(WriteUInt64), length, 64);
+            WriteVariableBytes(destination, value, offset, length, sizeof(ulong));
+        }
+
+        /// <summary>
+        /// Writes a specific number of bytes to the specified buffer.
+        /// </summary>
+        /// <param name="destination"> The destination buffer. </param>
+        /// <param name="value"> The container for the value. </param>
+        /// <param name="valueOffset"> The starting point (in bits). </param>
+        /// <param name="length"> The amount of bytes. </param>
+        /// <param name="destOffset"> The starting point (in bytes). </param>
+        public static void WriteBytes(byte[] destination, byte[] value, int valueOffset, int length, int destOffset)
+        {
+            int byteIndex = destOffset >> 3;
+            int firstPartLen = destOffset & 7;
+
+            if (firstPartLen == 0) Buffer.BlockCopy(value, valueOffset, destination, byteIndex, length);
+            else
+            {
+                int nextPartLen = 8 - firstPartLen;
+                for (int i = 0; i < length; i++)
+                {
+                    byte src = value[valueOffset + i];
+
+                    destination[byteIndex] &= (byte)(255 >> nextPartLen);
+                    destination[byteIndex++] |= (byte)(src << firstPartLen);
+
+                    destination[byteIndex] &= (byte)(255 << firstPartLen);
+                    destination[byteIndex] |= (byte)(src >> nextPartLen);
+                }
+            }
+        }
+
+        private static void WriteVariableBytes(byte[] destination, ulong value, int offset, int length, int numBytes)
+        {
+            for (int i = 0; i < numBytes && length > 0; i++, length -= 8, offset += 8)
+            {
+                if (length <= 8) WriteByte(destination, (byte)(value >> (i << 3)), offset, length);
+                else WriteByte(destination, (byte)(value >> (i << 3)), offset, 8);
+            }
+        }
+
+        private static void CheckOverflow(string func, int check, int max)
+        {
+            LoggedException.RaiseIf(check < 1 || check > max, nameof(BitWriter), $"{func} can only write between 1 and {max} bits");
+        }
+    }
+}
